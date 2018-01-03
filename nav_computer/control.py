4d538e1818f15f4ee8a4f262262201bb0357d00d
Andrew Benton <andrew.reed.benton@gmail.com>
2018-01-03 18:54:29 -0500

    incomplete: committed for physical testing

--- nav_computer/control.py
+++ nav_computer/control.py
@@ -1,82 +1,75 @@
+#!/usr/bin/env python3
+"""
+Control system for tennis ball collector.
+"""
+
+__author__ = "Andrew Benton"
+__version__ = "0.1.0"
+
 from serial import Serial
 import sys
 import time
 from math import atan, sqrt, pow
 import numpy as np
 
-import logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 def cart2pol(x, y):
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x) - np.pi/2 # 90 degree correction for headings
     return(rho, phi)
 
-class Controller(object):
-    forward_move_calibration = 1.31855
-    turning_move_calibration = 1.39195
+class ArduinoCommander(object):
 
     def __init__(self, port, baud):
         self.port = port
         self.baud = baud
         self.serial = Serial(self.port, self.baud, timeout=None)
-        self.curr_x = self.curr_y = self.curr_phi = 0
         time.sleep(2)
 
-    def set_position(self, x, y, phi):
-        logger.info("updating position to be x=%s y=%s phi=%s" % (x, y, phi))
-        self.curr_x, self.curr_y, self.curr_phi = x, y, phi
-
-    def reconfigure(self, forward_move_calibration=None, turning_move_calibration=None):
-        if forward_move_calibration:
-            self.forward_move_calibration = forward_move_calibration
-            logger.info("reconfigured forward_move_calibration=%s" % forward_move_calibration)
-        if turning_move_calibration:
-            self.turning_move_calibration = turning_move_calibration
-            logger.info("reconfigured turning_move_calibration=%s" % turning_move_calibration)
-
-    def _send(self, left_speed, right_speed):
+    def command(self, left_speed, right_speed):
+        left_speed, right_speed = int(left_speed), int(right_speed)
         message = "%d %d\r\n" % (left_speed, right_speed)
         self.serial.flush()
         self.serial.write(message.encode())
 
-    def move(self, left_speed, right_speed, move_time, stop=True):
-        self._send(left_speed, right_speed)
-        time.sleep(move_time)
-        if stop:
-            self._send(0, 0)
+class PointAndShootPlanner(object):
 
-    def move_many(self, moves, stop=True):
-        for left_speed, right_speed, move_time in moves:
-            self._send(left_speed, right_speed)
-            time.sleep(move_time)
-        if stop:
-            self._send(0, 0)
+    def __init__(self, min_speed=20, max_speed=150):
+        self.min_speed = min_speed
+        self.max_speed = max_speed
+        self.curr_left_speed = self.curr_right_speed = 0
 
-    def move_to(self, x, y, speed=50):
-        """ performs simple two phase moves: rotate and move forward. """
-        x_disp = x - self.curr_x
-        y_disp = y - self.curr_y
-        logger.info("recieved command x_disp=%s, y_disp=%s" % (x_disp, y_disp))
-        rho_disp, phi = cart2pol(x_disp, y_disp)
-        phi_disp = phi - self.curr_phi
-        self.curr_x, self.curr_y, self.curr_phi = x, y, phi
-        if not np.isclose(phi_disp, 0, atol=1e-1):
-            turning_move_time = self.turning_move_calibration*abs(phi_disp)
-            logger.info("executing turning move phi=%s radians, time=%s seconds" % (phi_disp, turning_move_time))
-            if phi_disp < 0:
-                self.send(-speed, speed, turning_move_time)
+    def _compute_trajectory(self, disp_x, disp_y):
+        #disp_rho = np.sqrt(disp_x**2 + disp_y**2)
+        disp_phi = np.arctan2(disp_y, disp_x) - np.pi/2 # 90 degree correction for headings
+        print(disp_phi)
+        if not np.isclose(disp_phi, 0, rtol=2e-1, atol=2e-1):
+            # if angle is significantly different, then turn
+            if disp_phi > 0:
+                return -1, 0
             else:
-                self.send(speed, -speed, turning_move_time)
-        if not np.isclose(rho_disp, 0):
-            forward_move_time = self.forward_move_calibration*abs(rho_disp)
-            logger.info("executing forward move rho=%s feet, time=%s seconds" % (rho_disp, forward_move_time))
-            self.send(speed, speed, forward_move_time)
-        return x, y, phi
+                return 0, -1
+        else:
+            return 1, 1
+
+    def plan(self, disp_x, disp_y):
+        left_dir, right_dir = self._compute_trajectory(disp_x, disp_y)
+        left_speed = self.speed*left_dir
+        right_speed = self.speed*right_dir
+        return left_speed, right_speed, move_time
+
+def move(port, baud):
+    controller = ArduinoController(port, baud)
+    planner = PointAndShootPlanner()
+    while True:
+        object_points = yield # they're called coroutines and I just learned about them too
+        print(object_points)
+        left_speed, right_speed = planner.plan(object_points)
+        print(left_speed, right_speed)
+        controller.control(left_speed, right_speed)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
+    import sys
 
     parser = ArgumentParser(description="Listen for commands to execute on robot.")
     parser.add_argument("--port",  dest="port", type=str, default="/dev/ttyACM0",
@@ -85,13 +78,21 @@ if __name__ == "__main__":
                         help="baud rate of motor controller.")
     args = parser.parse_args()
 
-    moves = [
-        ( 50,  50, 5),
-        (100, 100, 5),
-        (150, 150, 5),
-        (200, 200, 5),
-        (250, 250, 5) 
-    ]
-
-    controller = Controller(args.port, args.baud)
-    controller.move_many(moves)
+    commander = ArduinoCommander(args.port, args.baud)
+    planner = PointAndShootPlanner(args.speed)
+    while True:
+        try:
+            raw_instructions = input("robot: ")
+            if "exit" in raw_instructions:
+                break
+            command_level, arg_1, arg_2 = raw_instructions.split()
+            if "speed" in command_level:
+                left_speed, right_speed = arg_1, arg_2
+                commander.command(left_speed, right_speed)
+            #elif "point" in command_level:
+            #    disp_x, disp_y = arg_1, arg_2
+            #    left_speed, right_speed, move_time = planner.plan(disp_x, disp_y)
+        except BaseException as e:
+            print(e)
+            break
+    commander.command(0, 0)
